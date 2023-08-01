43fef376d1c615e25a41440a3ba564839d73d126
NFuenzalida <n.fuenzalida.z@gmail.com>
2023-08-01 19:57:02 -0400

    Modelos iniciales

---
+++ server/src/models/Producto.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Producto",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      precio: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    { timestamps: false, freezeTableName: true }
+  );
+};
---
+++ server/src/models/Usuario.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Usuario",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      fono: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: {
+            msg: "Email invalido",
+          },
+        },
+      },
+    },
+    { timestamps: false, freezeTableName: true }
+  );
+};
