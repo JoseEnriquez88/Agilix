b7e9615abbf61a66f38412e2594abada2627db0f
Djoanjaramillo <joanjaramillo_9@hotmail.com>
2023-08-02 10:37:51 -0500

    cree algo de las card y estoy probando el push

--- Client/src/Componentes/productos/productos.jsx
+++ Client/src/Componentes/productos/productos.jsx
@@ -1,17 +1,14 @@
-// import { useSelector } from 'react-redux'
-// export const cosas = () => {
-//     const producto = useSelector(state => state.producto)
-//     return (
-//         <cosas>
-//             <h1>nombre: {producto.nombre}</h1>
-//             <h1><img src="" alt="" />: {producto.img}</h1>
-//             <h1>precio: {producto.precio}</h1>
-//         </cosas>
-//     )
-// }
-<div>
+
+import styles from './productos.module.css'
+const Productos = () => {
+    return (
+    
+<div className={styles.cards}>
     <h1>imagen</h1>
     <h2>producto</h2>
     <h3>precio</h3>
     
 </div>
+ )
+}
+export default Productos;
