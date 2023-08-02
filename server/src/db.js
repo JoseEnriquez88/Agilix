6d37b9f924de744be503bde5191fea7046261f22
NFuenzalida <n.fuenzalida.z@gmail.com>
2023-08-01 22:07:43 -0400

    Modificacion archivo SB, carpetas

--- server/src/db.js
+++ server/src/db.js
@@ -1,38 +1,68 @@
-const { Sequelize,Datatypes } = require('sequelize');
-const Usuario = require ('./models/Usuario');
-const Cliente = require('./models/Cliente')
-const Producto= require ('./models/Producto')
-const Venta= require('./models/Venta')
+const { Sequelize, DataTypes } = require("sequelize");
+
+const fs = require("fs");
+const path = require("path");
+
 //requerimos dotenv
-require('dotenv').config();
+require("dotenv").config();
+
 //obtenemos las variables del env
-const {DB_USER,DB_PASSWORD,DB_HOST,DB_PORT,DB_NAME,DB_DIALECT}=process.env
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DIALECT } = process.env;
 
 const sequelize = new Sequelize(
-`${DB_DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}` 
+  `${DB_DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  {
+    logging: false, // oculta la info de cada query que se ejecuta desde postgres
+    native: false, // ~30% mejora de rendimiento
+  }
 );
-// ejecutamos las funciones de las tablas
-Usuario(sequelize);
-Cliente(sequelize);
-Producto(sequelize);
-Venta(sequelize);
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
+
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach((model) => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
+
+//extraemos los modelos
+sequelize.models = Object.fromEntries(capsEntries);
+
 // aca sacamos el modelo literal,con las propiedades de sequelize para poder hacer relaciones
-const {Usuario,Cliente,Producto,Venta } = sequelize.models;
+const { Usuario, Cliente, Producto, Venta } = sequelize.models;
 
 //relacion de uno a uno
 
-Venta.belongsTo(Cliente)
-Venta.belongsTo(Usuario)
+Venta.belongsTo(Cliente);
+Venta.belongsTo(Usuario);
 
 //relacion uno a muchos
-Cliente.hasMany(Venta)
-Usuario.hasMany(Venta)
+Cliente.hasMany(Venta);
+Usuario.hasMany(Venta);
 
 //relacion de muchos a muchos
-const Detalle_Venta = sequelize.define('Detalle_Venta',{cantidad:Datatypes.INTEGER},{timestamps:false});
-Producto.belongsToMany(Venta,{through:Detalle_Venta});
-Venta.belongsToMany (Producto, {through:Detalle_Venta});
+const Detalle_Venta = sequelize.define(
+  "Detalle_Venta",
+  { cantidad: DataTypes.INTEGER },
+  { timestamps: false }
+);
+Producto.belongsToMany(Venta, { through: Detalle_Venta });
+Venta.belongsToMany(Producto, { through: Detalle_Venta });
 
-module.exports ={
-    conn: sequelize,
+module.exports = {
+  conn: sequelize,
 };
