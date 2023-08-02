568c32b40d2bfc692b65c69d012437119e17279d
Djoanjaramillo <joanjaramillo_9@hotmail.com>
2023-08-02 10:03:00 -0500

    componente

---
+++ Client/src/Componentes/productos.jsx
@@ -0,0 +1,17 @@
+// import { useSelector } from 'react-redux'
+// export const cosas = () => {
+//     const producto = useSelector(state => state.producto)
+//     return (
+//         <cosas>
+//             <h1>nombre: {producto.nombre}</h1>
+//             <h1><img src="" alt="" />: {producto.img}</h1>
+//             <h1>precio: {producto.precio}</h1>
+//         </cosas>
+//     )
+// }
+<div>
+    <h1>imagen</h1>
+    <h2>producto</h2>
+    <h3>precio</h3>
+    
+</div>
