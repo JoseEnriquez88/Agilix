857a90b7dbc1098700bd0a8c8284d9e3f40ec97e
Djoanjaramillo <joanjaramillo_9@hotmail.com>
2023-08-02 14:07:07 -0500

    todo nuevo

--- Client/src/Componentes/productos/productos.jsx
+++ Client/src/Componentes/productos/productos.jsx
@@ -1,37 +1,60 @@
-import imagen from './ejemplo.png'
+import cocacola from '../../assets/productos/Coca-Cola-PNG-Photos.png'
+import papa from '../../assets/productos/papa-tortilla-cubo-e1682068141538.png'
+import jamon from '../../assets/productos/Ham-PNG-Free-File-Download.png'
+import jabon from '../../assets/productos/jabon.png'
+import Pasta from '../../assets/productos/Pasta.png'
+import shampoo from '../../assets/productos/shampoo.png'
 import styles from './productos.module.css'
 const Productos = () => {
     return (
   <div className={styles.contenedor}>
 <div className={styles.cards}>
-    <img  className={styles.imagen} src={imagen} alt=""/>
+    <img  className={styles.imagen} src={cocacola} alt=""/>
     <div className={styles.contenedorPP}>
-    <h2 className={styles.nombre}>producto</h2>
-    <h3 className={styles.precio}>50$</h3>
+    <h2 className={styles.nombre}>Coca - Cola</h2>
+    <h3 className={styles.precio}>1.5 $</h3>
     </div>
 </div>
 <div className={styles.cards}>
-    <img  className={styles.imagen} src={imagen} alt=""/>
+    <img  className={styles.imagen} src={papa} alt=""/>
     <div className={styles.contenedorPP}>
-    <h2 className={styles.nombre}>producto</h2>
-    <h3 className={styles.precio}>80$</h3>
+    <h2 className={styles.nombre}>Papa</h2>
+    <h3 className={styles.precio}>0.5 $</h3>
     </div>
 </div>
 <div className={styles.cards}>
-    <img  className={styles.imagen} src={imagen} alt=""/>
+    <img  className={styles.imagen} src={jamon} alt=""/>
     <div className={styles.contenedorPP}>
-    <h2 className={styles.nombre}>producto</h2>
-    <h3 className={styles.precio}>120$</h3>
+    <h2 className={styles.nombre}>Jamón (1kg)</h2>
+    <h3 className={styles.precio}>5 $</h3>
     </div>
 </div>
+
 <div className={styles.cards}>
-    <img  className={styles.imagen} src={imagen} alt=""/>
+    <img  className={styles.imagen} src={jabon} alt=""/>
     <div className={styles.contenedorPP}>
-    <h2 className={styles.nombre}>producto</h2>
-    <h3 className={styles.precio}>190$</h3>
+    <h2 className={styles.nombre}>Jabon (Protex)</h2>
+    <h3 className={styles.precio}>5 $</h3>
     </div>
 </div>
-</div>   
+
+<div className={styles.cards}>
+    <img  className={styles.imagen} src={Pasta} alt=""/>
+    <div className={styles.contenedorPP}>
+    <h2 className={styles.nombre}>Pasta (Barilla)</h2>
+    <h3 className={styles.precio}>3.5 $</h3>
+    </div>
+</div>
+
+<div className={styles.cards}>
+    <img  className={styles.imagen} src={shampoo} alt=""/>
+    <div className={styles.contenedorPP}>
+    <h2 className={styles.nombre}>Shampoo(Dove)</h2>
+    <h3 className={styles.precio}>1.5 $</h3>
+    </div>
+</div>
+
+</div>
 
 
 
