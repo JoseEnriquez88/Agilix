781e21c9f64a5ab1e0d45f871537278da9853d8d
kevincalle3101 <kevinmendoza3101@hotmail.com>
2023-08-03 13:21:25 -0500

    cambio en las importaciones de archivos

--- Client/src/Componentes/CUENTAUSUARIO/Actualizaciones/Actualizaciones.jsx
+++ Client/src/Componentes/CUENTAUSUARIO/Actualizaciones/Actualizaciones.jsx
@@ -4,7 +4,7 @@ import styles from './Actualizaciones.module.css'
  const Actualizaciones = () => {
     return(
         <div>
-            <h1>Actualizaciones</h1>
+            <h1>Actualizacion</h1>
         </div>
     )
 }
