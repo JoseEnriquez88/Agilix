8837a7d9a77ca1d0ccc010c4263cc879b355b08c
Julian Andres Sosa <julian.m.sosa.muchut@gmail.com>
2023-08-02 22:28:13 -0300

    hice el crud de productos

--- server/src/models/Producto.js
+++ server/src/models/Producto.js
@@ -19,7 +19,7 @@ module.exports = (sequelize) => {
         allowNull: false,
       },
       precio: {
-        type: DataTypes.STRING,
+        type: DataTypes.FLOAT,
         allowNull: false,
       },
     },
--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -2,11 +2,14 @@ const { Router } = require("express");
 
 const getAllProductsHandler = require("../handlers/productos/getAllProductsHandler");
 const postProductHandler = require("../handlers/productos/postProductHandler");
+const getByIdProductHandler = require("../handlers/productos/getByIdProductHandler");
 
 const router = Router();
 
 //!get de productos
 router.get("/productos", getAllProductsHandler);
+//! get de productos por id
+router.get("/productos/:id",getByIdProductHandler);
 
 //!post de productos
 router.post("/crearProducto", postProductHandler);
