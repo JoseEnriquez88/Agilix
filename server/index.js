f88ccaf2b0f3436336e6c42087c2127fb4baf50d
Julian Andres Sosa <julian.m.sosa.muchut@gmail.com>
2023-08-03 20:21:17 -0300

    modificacion de modelo productos

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const { conn } = require('./src/db');
 const PORT = 3001;
 
 server.listen(PORT, () => {
-    conn.sync({ force: true });
+    conn.sync({ force: false });
     console.log('Servidor escuchando en el puerto:', PORT);
 })
