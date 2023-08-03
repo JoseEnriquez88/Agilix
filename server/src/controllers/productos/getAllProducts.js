33ef7d66d5f8b3a9c8f56ddd980d9d2415b7c0ac
Djoanjaramillo <joanjaramillo_9@hotmail.com>
2023-08-03 17:48:34 -0500

    acabo de mejorar el css del componente , mejor dicho hice el 5% que le faltaba a niko

--- server/src/controllers/productos/getAllProducts.js
+++ server/src/controllers/productos/getAllProducts.js
@@ -2,7 +2,7 @@ const { Producto } = require("../../db");
 
 const getAllProducts = async () => {
     const productos = await Producto.findAll({
-        attributes: ['nombre', 'img', 'precio'],
+        attributes: ['id','nombre', 'img', 'precio'],
     });
     if(productos.length === 0) throw new Error('No se encontraron productos para mostrar.'); 
     return productos;
