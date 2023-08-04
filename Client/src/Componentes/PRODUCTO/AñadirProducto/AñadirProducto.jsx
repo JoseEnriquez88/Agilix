b7f78847ad80ed6f0648b73bbfd14dc63363d329
NFuenzalida <n.fuenzalida.z@gmail.com>
2023-08-04 16:51:05 -0400

    Estandarizacion de rama main

--- Client/src/Componentes/PRODUCTO/AñadirProducto/AñadirProducto.jsx
+++ Client/src/Componentes/PRODUCTO/AñadirProducto/AñadirProducto.jsx
@@ -1,88 +1,83 @@
-import { useState } from 'react';
+import { useState } from "react";
 // import { useDispatch, useSelector } from 'react-redux';
 // import { setNombre, setPrecio, setImg, crearProducto } from '../../../Redux/crearProductoSlice';
-import styles from '../AñadirProducto/AñadirProducto.module.css';
-import axios from 'axios'
+import styles from "../AñadirProducto/AñadirProducto.module.css";
+import axios from "axios";
 
 const InitialCreate = {
-  nombre: '',
-  precio: '',
-  img: '',
+  nombre: "",
+  precio: "",
+  img: "",
 };
-
-
 
 const AñadirProducto = () => {
   // const dispatch = useDispatch();
   // const { nombre, precio, img } = useSelector((state) => state.productoCreado);
   // const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [input, setInput] = useState (InitialCreate)
-
+  const [input, setInput] = useState(InitialCreate);
 
   const handleChange = (event) => {
     // dispatch(setNombre(event.target.value));
-    setInput({...input, [event.target.name]: event.target.value})
+    setInput({ ...input, [event.target.name]: event.target.value });
 
+    // const handlePriceChange = (event) => {
+    //   dispatch(setPrecio(event.target.value));
+    // };
+  };
 
-  // const handlePriceChange = (event) => {
-  //   dispatch(setPrecio(event.target.value));
-  // };
-
-  
-
-};
-
-const handleImageChange = (event) => {
-  const file = event.target.files[0];
-  setInput({...input, [event.target.name]: event.target.value})
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setInput({ ...input, [event.target.name]: event.target.value });
     // const previewImage = document.getElementById('preview');
     // previewImage.src = file ? URL.createObjectURL(file) : '#';
-    // previewImage.style.display = file ? 'block' : 'none';  
-  }
+    // previewImage.style.display = file ? 'block' : 'none';
+  };
 
   const handleRemoveImage = () => {
     // Limpiar el input de tipo "file" para permitir seleccionar otra imagen
-    const fileInput = document.getElementById('image');
-    fileInput.value = '';
+    const fileInput = document.getElementById("image");
+    fileInput.value = "";
 
     // Restablecer la vista previa
-    const previewImage = document.getElementById('preview');
-    previewImage.src = '#';
-    previewImage.style.display = 'none';
+    const previewImage = document.getElementById("preview");
+    previewImage.src = "#";
+    previewImage.style.display = "none";
 
     // Eliminar la imagen del estado Redux
-    setInput({...input, img: ''});
+    setInput({ ...input, img: "" });
   };
-
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     console.log(input);
     try {
-      const response = await axios.post (`http://localhost:3001/crearProducto`, input)
+      const response = await axios.post(
+        `http://localhost:3001/productos`,
+        input
+      );
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-    // // Crea un objeto formData con los datos del producto
-    // const formData = new FormData();
-    // formData.append('nombre', nombre);
-    // formData.append('precio', precio);
-    // formData.append('img', img);
+  // // Crea un objeto formData con los datos del producto
+  // const formData = new FormData();
+  // formData.append('nombre', nombre);
+  // formData.append('precio', precio);
+  // formData.append('img', img);
 
-    // // Llama al Thunk para enviar los datos al backend
-    // await dispatch(crearProducto(formData));
+  // // Llama al Thunk para enviar los datos al backend
+  // await dispatch(crearProducto(formData));
 
-    // // Mostrar el mensaje de éxito
-    // setShowSuccessMessage(true);
+  // // Mostrar el mensaje de éxito
+  // setShowSuccessMessage(true);
 
-    // // Ocultar el mensaje después de un breve tiempo
-    // setTimeout(() => {
-    //   setShowSuccessMessage(false);
-    // }, 2000);
+  // // Ocultar el mensaje después de un breve tiempo
+  // setTimeout(() => {
+  //   setShowSuccessMessage(false);
+  // }, 2000);
 
-    // Limpiar los campos del formulario después de un breve tiempo
+  // Limpiar los campos del formulario después de un breve tiempo
   //   setTimeout(() => {
   //     dispatch(setNombre(''));
   //     dispatch(setPrecio(''));
@@ -90,21 +85,47 @@ const handleImageChange = (event) => {
   //   }, 2500);
   // };
 
-
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <div>
         <p className={styles.titituloForm}>Crear Productos</p>
-        <label className={styles.inputGropLabel} htmlFor="name">Nombre: </label>
-        <input className={styles.inputGroup} type="text" id="name" name='nombre' value={input.nombre} onChange={handleChange} /> 
+        <label className={styles.inputGropLabel} htmlFor="name">
+          Nombre:{" "}
+        </label>
+        <input
+          className={styles.inputGroup}
+          type="text"
+          id="name"
+          name="nombre"
+          value={input.nombre}
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <label className={styles.inputGropLabel} htmlFor="price">Precio: </label>
-        <input className={styles.inputGroup}type="text" id="price" name='precio' value={input.precio} onChange={handleChange} /> 
+        <label className={styles.inputGropLabel} htmlFor="price">
+          Precio:{" "}
+        </label>
+        <input
+          className={styles.inputGroup}
+          type="text"
+          id="price"
+          name="precio"
+          value={input.precio}
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <label className={styles.inputGropLabel} htmlFor="image">Imagen: </label>
-        <input className={styles.inputGroup}type="file" id="image" name='img' value={input.img} onChange={handleImageChange} />
+        <label className={styles.inputGropLabel} htmlFor="image">
+          Imagen:{" "}
+        </label>
+        <input
+          className={styles.inputGroup}
+          type="file"
+          id="image"
+          name="img"
+          value={input.img}
+          onChange={handleImageChange}
+        />
         <br />
         <br />
         {/* {input.img && (
@@ -113,12 +134,24 @@ const handleImageChange = (event) => {
         
         )} */}
       </div>
-      <button className={styles.buttonDelete} type="button" onClick={handleRemoveImage}>Eliminar imagen</button>
+      <button
+        className={styles.buttonDelete}
+        type="button"
+        onClick={handleRemoveImage}
+      >
+        Eliminar imagen
+      </button>
       <br />
-      <button className={styles.buttonCreate} type="submit" onClick={handleSubmit}>Crear Producto</button>
-        {/* {showSuccessMessage && <div className="success-modal">¡Producto creado exitosamente!</div>} */}
+      <button
+        className={styles.buttonCreate}
+        type="submit"
+        onClick={handleSubmit}
+      >
+        Crear Producto
+      </button>
+      {/* {showSuccessMessage && <div className="success-modal">¡Producto creado exitosamente!</div>} */}
     </form>
   );
-}
+};
 
 export default AñadirProducto;
