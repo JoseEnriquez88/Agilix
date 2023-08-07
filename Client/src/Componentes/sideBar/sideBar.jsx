78961012cdbcd66ea58ca76632db6aa85ba8e78d
kevincalle3101 <kevinmendoza3101@hotmail.com>
2023-08-07 17:50:45 -0500

    Mejoras Sidebar-Kev

--- Client/src/Componentes/sideBar/sideBar.jsx
+++ Client/src/Componentes/sideBar/sideBar.jsx
@@ -41,77 +41,89 @@ export const Sidebar = () => {
         {anchoReducido ? "A" : "Agilix"}
       </h1>
       <div
-        className={
-          anchoReducido ? styles.contenedorReducido : styles.contenedor
-        }
+        className={anchoReducido ? styles.contenedorReducido : styles.contenedor}
       >
-        {!anchoReducido && (
+        {!anchoReducido ? (
           <>
             <h5>CUENTA / USUARIO</h5>
-            <NavLink className={styles.NavLink} to="/">
-              General
+            <NavLink className={styles.NavLink} to="/" onClick ={cambiarDireccionFlecha} >
               <HomeIcon className={styles.iconSideBar} />
+              General
             </NavLink>
-            <NavLink className={styles.NavLink} to="/cuentas">
-              Cuentas
+            <NavLink className={styles.NavLink} to="/cuentas" onClick ={cambiarDireccionFlecha}>
               <AttachMoneyIcon className={styles.iconSideBar} />
+              Cuentas
             </NavLink>
-            <NavLink className={styles.NavLink} to="/clientes">
-              Clientes
+            <NavLink className={styles.NavLink} to="/clientes" onClick ={cambiarDireccionFlecha}>
               <SupervisorAccountIcon className={styles.iconSideBar} />
+              Clientes
             </NavLink>
-            <NavLink className={styles.NavLink} to="/configuracion">
-              Configuración
+            <NavLink className={styles.NavLink} to="/configuracion" onClick ={cambiarDireccionFlecha}>
               <SettingsIcon className={styles.iconSideBar} />
+              Configuración
             </NavLink>
+          </>
+        ):
+        (
+          <>
+            <HomeIcon className={styles.iconSideBarReducido} />
+            <AttachMoneyIcon className={styles.iconSideBarReducido} />
+            <SettingsIcon className={styles.iconSideBarReducido} />
           </>
         )}
       </div>
-      <div
-        className={
-          anchoReducido ? styles.contenedorReducido : styles.contenedor
-        }
+      <div className={anchoReducido ? styles.contenedorReducido : styles.contenedor}
       >
-        {!anchoReducido && (
+        {!anchoReducido ? (
           <>
             <h5>PRODUCTOS</h5>
-            <NavLink className={styles.NavLink} to="/añadirProducto">
-              Añadir Producto
+            <NavLink className={styles.NavLink} to="/añadirProducto" onClick ={cambiarDireccionFlecha}>
               <AddCircleIcon className={styles.iconSideBar} />
+              Añadir Producto
             </NavLink>
-            <NavLink className={styles.NavLink} to="/misProductos">
-              Mis Productos
+            <NavLink className={styles.NavLink} to="/misProductos" onClick ={cambiarDireccionFlecha}>
               <LiquorIcon className={styles.iconSideBar} />
+              Mis Productos
             </NavLink>
-            <NavLink className={styles.NavLink} to="/inventario">
-              Inventario
+            <NavLink className={styles.NavLink} to="/inventario" onClick ={cambiarDireccionFlecha}>
               <InventoryIcon className={styles.iconSideBar} />
+              Inventario
             </NavLink>
+          </>
+        ):
+        (
+          <>
+            <AddCircleIcon className={styles.iconSideBarReducido} />
+            <LiquorIcon className={styles.iconSideBarReducido} />
+            <InventoryIcon className={styles.iconSideBarReducido} />
           </>
         )}
       </div>
       <div
-        className={
-          anchoReducido
-            ? styles.contenedorUsuariosReducido
-            : styles.contenedorUsuarios
-        }
+        className={ styles.contenedorUsuarios }
       >
-        {!anchoReducido && (
+        {!anchoReducido ? (
           <>
             <h5>USUARIOS</h5>
-            <NavLink className={styles.NavLink} to="/configPerfil">
-              Configuración de Perfil
+            <NavLink className={styles.NavLink} to="/configPerfil" onClick ={cambiarDireccionFlecha}>
               <PersonIcon className={styles.iconSideBar} />
+              Configuración de Perfil
             </NavLink>
-            <NavLink className={styles.NavLink} to="/admin">
-              Administrar Usuario
+            <NavLink className={styles.NavLink} to="/admin" onClick ={cambiarDireccionFlecha}>
               <ManageAccountsIcon className={styles.iconSideBar} />
+              Administrar Usuario
             </NavLink>
-            <NavLink
-            className={styles.NavLink}>Cerrar sesión
-            <LogoutIcon className={styles.iconSideBar} />
+            <NavLink className={styles.NavLink} onClick ={cambiarDireccionFlecha}>
+              <LogoutIcon className={styles.iconSideBar} />
+              Cerrar sesión
             </NavLink>
+          </>
+        ):
+        (
+          <>
+            <PersonIcon className={styles.iconSideBarReducido}/>
+            <ManageAccountsIcon className={styles.iconSideBarReducido} />
+            <LogoutIcon className={styles.iconSideBarReducido} />
           </>
         )}
       </div>
