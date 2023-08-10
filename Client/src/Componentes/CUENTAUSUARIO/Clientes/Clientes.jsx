32f545e45c3e004da2b6e282ab5d606c7877d361
NFuenzalida <n.fuenzalida.z@gmail.com>
2023-08-10 16:07:17 -0400

    Adicion modulo react en todos los componentes

--- Client/src/Componentes/CUENTAUSUARIO/Clientes/Clientes.jsx
+++ Client/src/Componentes/CUENTAUSUARIO/Clientes/Clientes.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchClientes } from "../../../Redux/clientesSlice";
@@ -16,50 +17,48 @@ const Clientes = () => {
   if (!clientes.allClientes) {
     return (
       <div className={styles.buttonContainer}>
-      <button className={styles.CrearCliente}>
-        <NavLink to='/crearcliente' style={{ textDecoration: 'none' }}>
-          Crear Cliente
-        </NavLink>
-      </button>
-
+        <button className={styles.CrearCliente}>
+          <NavLink to="/crearcliente" style={{ textDecoration: "none" }}>
+            Crear Cliente
+          </NavLink>
+        </button>
       </div>
-    )
+    );
   }
   return (
     <div>
       <h1 className={styles.tittle}>Lista de Clientes</h1>
-    <div className={styles.container}>
-      <button className={styles.CrearCliente}>
-        <NavLink to='/crearcliente' style={{ textDecoration: 'none' }}>
-          Crear Cliente
-          {/* <PersonAddIcon className={styles.icon} /> */}
-        </NavLink>
-      </button>
+      <div className={styles.container}>
+        <button className={styles.CrearCliente}>
+          <NavLink to="/crearcliente" style={{ textDecoration: "none" }}>
+            Crear Cliente
+            {/* <PersonAddIcon className={styles.icon} /> */}
+          </NavLink>
+        </button>
 
-      <ol className={styles.clienteList}>
-        {clientes.allClientes.map((cliente) => (
-          <li className={styles.clienteRow} key={cliente.id}>
-            <div className={styles.clienteInfo}>
-              <span className={styles.tituloPrincipal}>Nombre:</span>
-              <p className={styles.nombreCliente}>{cliente.nombre} {cliente.apellido}</p>
-            </div>
-            <div className={styles.clienteInfo}>
-              <span className={styles.tituloPrincipal}>Teléfono:</span>
-              <p className={styles.telefonoCliente}>{cliente.telefono}</p>
-            </div>
-            <div className={styles.clienteInfo}>
-              <span className={styles.tituloPrincipal}>Email:</span>
-              <p className={styles.clienteEmail}>{cliente.email}</p>
-            </div>
-          </li>
-        ))}
-      </ol>
-    </div>
+        <ol className={styles.clienteList}>
+          {clientes.allClientes.map((cliente) => (
+            <li className={styles.clienteRow} key={cliente.id}>
+              <div className={styles.clienteInfo}>
+                <span className={styles.tituloPrincipal}>Nombre:</span>
+                <p className={styles.nombreCliente}>
+                  {cliente.nombre} {cliente.apellido}
+                </p>
+              </div>
+              <div className={styles.clienteInfo}>
+                <span className={styles.tituloPrincipal}>Teléfono:</span>
+                <p className={styles.telefonoCliente}>{cliente.telefono}</p>
+              </div>
+              <div className={styles.clienteInfo}>
+                <span className={styles.tituloPrincipal}>Email:</span>
+                <p className={styles.clienteEmail}>{cliente.email}</p>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 };
 
 export default Clientes;
-
-
-
