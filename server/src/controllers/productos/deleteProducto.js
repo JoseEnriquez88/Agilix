92e6746ee57b46da8813c132ad76a61399a21d26
RServigni <123337791+RaiWorldz@users.noreply.github.com>
2023-08-10 10:36:22 -0400

    Update deleteProducto.js
    Eliminado console.log

--- server/src/controllers/productos/deleteProducto.js
+++ server/src/controllers/productos/deleteProducto.js
@@ -1,15 +1,13 @@
 const { Producto } = require('../../db');
 
 const deleteProducto = async (id,estado) => {
-    console.log(id)
+    
     const producto = await Producto.findOne({ where: { id:id } });
     console.log(id,"este es el producto nro: ",producto)
     if(!producto) throw new Error(`El producto ${id} no existe`);
-    console.log(estado)
     producto.estado= estado;
     await producto.save();
-    console.log(id,"este es el producto nro: ",producto)
-
+    
 
     if(!producto) throw new Error(`El producto con el id ${id} no pudo eliminarse.`);
     return `El producto ${producto.name} fue eliminado exitosamente.`;
