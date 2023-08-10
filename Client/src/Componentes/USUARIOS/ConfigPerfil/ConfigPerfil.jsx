3b870f2f3d6b4dea3744d7916d50960a82ef3567
kevincalle3101 <kevinmendoza3101@hotmail.com>
2023-08-10 14:43:52 -0500

    avances en el css de en configPerfil

--- Client/src/Componentes/USUARIOS/ConfigPerfil/ConfigPerfil.jsx
+++ Client/src/Componentes/USUARIOS/ConfigPerfil/ConfigPerfil.jsx
@@ -1,1 +1,18 @@
+import styles from './ConfigPerfil.module.css';
 
+const ConfigPerfil = () => {
+    return (
+        <div className={styles.contenedorPrincipal}>
+            <div className={styles.contenedorSecundario} >
+                <div className={styles.contenedorPerfil}>
+                    <h1>Hola</h1>
+                </div>
+                <div className={styles.contenedorConfigPerfil}>
+                    <h1>Hola2</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ConfigPerfil;
