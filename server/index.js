44d3d88f53a6a0b134f37496b06a1d66f0a4d37d
kevincalle3101 <kevinmendoza3101@hotmail.com>
2023-08-11 22:32:46 -0500

    Componente config perfil-kev

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db");
 
 const PORT = 3001;
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log("Servidor escuchando en el puerto:", PORT);
   });
