a5a66fa48bc0106fb2be81e95485ecdaf9058015
Mateo Pinto Giacosa <mateo.p.giacosa@gmail.com>
2023-08-14 14:45:43 -0300

    implementación de pagos con qr

--- server/src/handlers/Pagos/generar_ordenHandler.js
+++ server/src/handlers/Pagos/generar_ordenHandler.js
@@ -1,12 +1,13 @@
 const generar_orden = require("../../controllers/Pagos/generar_orden");
 
 const generar_ordenHandler = async (req, res) => {
-    const { items } = req.body;
+    const { InfoCarrito } = req.body;
+    console.log("Esto llegó al handler: " + InfoCarrito);
     try {
-       const orden = await generar_orden(items);
-        res.status(201).json(orden); 
+       const orden = await generar_orden(InfoCarrito);
+        res.status(201).json({ orden }); 
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        res.status(400).json({ error: "Error al generar la orden" + error.message });
     }
 };
 
