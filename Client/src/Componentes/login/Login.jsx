8165748575fd69c39201bbd868340cf6b8fff286
NFuenzalida <n.fuenzalida.z@gmail.com>
2023-08-24 08:30:17 -0400

    Aplicacion de auth0 para autenticacion de terceros

--- Client/src/Componentes/login/Login.jsx
+++ Client/src/Componentes/login/Login.jsx
@@ -1,46 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
-import GoogleIcon from '@mui/icons-material/Google';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Login() {
-  const googleAuth = () => {
-    window.open(
-      `${import.meta.env.VITE_API_URL}/auth/google/callback`,
-      "_self"
-    );
-  };
+  const { loginWithRedirect } = useAuth0();
+  // const googleAuth = () => {
+  //   window.open(
+  //     `${import.meta.env.VITE_API_URL}/auth/google/callback`,
+  //     "_self"
+  //   );
+  // };
   return (
-    <div className={styles.container}>
-      <h1 className={styles.heading}>
-        Bienvenidos a <span className={styles.AgilixTittle}> Agilix</span>
-      </h1>
-      <div className={styles.form_container}>
-        <div className={styles.left}></div>
-        <div className={styles.right}>
-          <h2 className={styles.from_heading}>Iniciar sesión</h2>
-          <input type="text" className={styles.input} placeholder="Email" />
-          <input
-            type="text"
-            className={styles.input}
-            placeholder="Contraseña"
-          />
-          <button className={styles.btn}>Iniciar sesión</button>
-          <p className={styles.text}>O inicia sesión con Google</p>
-          <button className={styles.google_btn} onClick={googleAuth}>
-            <span className={styles.svg}>
-          <GoogleIcon/>
-          </span>
-          </button>
-          
-          <p className={styles.text}>
-            ¿Primera vez? <Link to="/signup">Registrate</Link>
-          </p>
-        </div>
-      </div>
-    </div>
+    <button onClick={() => loginWithRedirect()}>Log In</button>
+    // <button onClick={() => loginWithRedirect()}>Log In</button>
+    // <div className={styles.container}>
+    //   <h1 className={styles.heading}>
+    //     Bienvenidos a <span className={styles.AgilixTittle}> Agilix</span>
+    //   </h1>
+    //   <div className={styles.form_container}>
+    //     <div className={styles.left}></div>
+    //     <div className={styles.right}>
+    //       <h2 className={styles.from_heading}>Iniciar sesión</h2>
+    //       <input type="text" className={styles.input} placeholder="Email" />
+    //       <input
+    //         type="text"
+    //         className={styles.input}
+    //         placeholder="Contraseña"
+    //       />
+    //       <button className={styles.btn}>Iniciar sesión</button>
+    //       <p className={styles.text}>O</p>
+    //       <button className={styles.google_btn} onClick={googleAuth}>
+    //         <span>Registrarse con Google</span>
+    //       </button>
+    //       <p className={styles.text}>
+    //         ¿Primera vez? <Link to="/signup">Registrate</Link>
+    //       </p>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
 export default Login;
-
